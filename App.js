7d6aa482c9f29d690a8ce89a038e23404d7bfddb
Dixit Gogoi <dixitgogoi2017@gmail.com>
2024-03-14 16:08:21 +0530

    first commit

--- App.js
+++ App.js
@@ -1,69 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { View, Button, Text } from 'react-native';
-import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+import { StyleSheet, Text, View } from 'react-native';
+import GuestStackNavigator from './src/navigation/GuestStackNavigator';
+import { store } from './src/redux/Store';
+import { Provider } from 'react-redux';
 
-const GoogleSignInScreen = () => {
+const App = () => {
 
-  const [data, setdata] = useState("")
-
-  useEffect(() => {
-    GoogleSignin.configure({
-      webClientId: "665473376055-vu11a0kv9gt3bv9aos8pfhi3ui9p8ubs.apps.googleusercontent.com",
-      androidClientId: "665473376055-0u7eve9pug2irpvk9pjskgm1d09qemsc.apps.googleusercontent.com",
-      scopes: ['profile', 'email'],
-      offlineAccess: true,
-      scopes: ['profile', 'email']
-    });
-  }, []);
-
-  const signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      await GoogleSignin.signOut(); 
-      const userInfo = await GoogleSignin.signIn();
-      setdata(userInfo.user)
-      console.log(userInfo);
-    } catch (error) {
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        console.log('Sign in cancelled');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        console.log('Operation in progress');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        console.log('Play services not available');
-      } else { 
-        console.error(error);
-      }
-    }
-  };
-  console.log("datadatadata", data && data)
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Provider store={store}>
+      <GuestStackNavigator />
+    </Provider>
+  )
+}
 
-      {/* <GoogleSigninButton
-        style={{ width: 192, height: 48 }}
-        size={GoogleSigninButton.Size.Wide}
-        color={GoogleSigninButton.Color.Dark}
-        onPress={signIn}
-        hideIcon={true} 
-      /> */}
-      <GoogleSigninButton
-        style={{ width: 192, height: 48 }}
-        // size={23}
-        // color={GoogleSigninButton.Color.Dark}
-        onPress={signIn}
-        hideLogo={true}
-      />
+export default App
 
-      {data && (
-        <View>
-          <Text>User Data:</Text>
-          <Text>Name: {data.name}</Text>
-          <Text>Email: {data.email}</Text>
+const styles = StyleSheet.create({})
 
-        </View>
-      )}
-    </View>
-  );
-};
 
-export default GoogleSignInScreen;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import { View, Button, Text } from 'react-native';
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+
+// const GoogleSignInScreen = () => {
+
+//   const [data, setdata] = useState("")
+
+//   useEffect(() => {
+//     GoogleSignin.configure({
+//       webClientId: "665473376055-vu11a0kv9gt3bv9aos8pfhi3ui9p8ubs.apps.googleusercontent.com",
+//       androidClientId: "665473376055-0u7eve9pug2irpvk9pjskgm1d09qemsc.apps.googleusercontent.com",
+//       scopes: ['profile', 'email'],
+//       offlineAccess: true,
+//       scopes: ['profile', 'email']
+//     });
+//   }, []);
+
+//   const signIn = async () => {
+//     try {
+//       await GoogleSignin.hasPlayServices();
+//       await GoogleSignin.signOut(); 
+//       const userInfo = await GoogleSignin.signIn();
+//       setdata(userInfo.user)
+//       console.log(userInfo);
+//     } catch (error) {
+//       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+//         console.log('Sign in cancelled');
+//       } else if (error.code === statusCodes.IN_PROGRESS) {
+//         console.log('Operation in progress');
+//       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+//         console.log('Play services not available');
+//       } else { 
+//         console.error(error);
+//       }
+//     }
+//   };
+//   console.log("datadatadata", data && data)
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <GoogleSigninButton
+//         style={{ width: 192, height: 48 }}
+//         // size={23}
+//         // color={GoogleSigninButton.Color.Dark}
+//         onPress={signIn}
+//         hideLogo={true}
+//       />
+//     </View>
+//   );
+// };
+
+// export default GoogleSignInScreen;
